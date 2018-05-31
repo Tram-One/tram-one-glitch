7dabe3a24600b243c824f1f15e91f45c97930c2f
jjurman <jjurman@constantcontact.com>
2018-05-31 00:12:28 -0400

    Publish from tram-one-express

---
+++ pages/home.js
@@ -0,0 +1,16 @@
+const Tram = require('tram-one')
+const html = Tram.html({
+  'app-header': require('../elements/app-header')
+})
+
+module.exports = () => {
+  return html`
+    <div>
+      <app-header />
+      <div>
+        Thank you for using Tram-One!<br />
+        To get started, edit <code>tram-one-glitch/pages/home.js</code>.
+      </div>
+    </div>
+  `
+}
---
+++ specs/header-spec.js
@@ -0,0 +1,8 @@
+const header = require('../elements/app-header')
+
+describe('header', () => {
+  it('should have the title', () => {
+    const page = header()
+    expect(page.outerHTML).toMatch('tram-one-glitch')
+  })
+})
